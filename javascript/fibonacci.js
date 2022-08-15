921e04ccb0403d28e18dbee324b42bafe98ab6ca
Pete Fowler <fowler.pete@gmail.com>
2022-08-15 16:26:37 -0600

    Add loop and recursive solutions

--- javascript/fibonacci.js
+++ javascript/fibonacci.js
@@ -1,6 +1,31 @@
 function fibonacci(num) {
-  // type your code here
+  // let a = 0;
+  // let b = 1;
+  // let c;
+  // if (num === 0) {return 0};
+  // for(let i = 1; i < num; i++) {
+  //   c = a + b;
+  //   a = b;
+  //   b = c;
+  // }
+  // return c;
+  if(num < 2) {
+    return num;
+  }
+  else {
+    return fibonacci(num-1) + fibonacci(num - 2);
+  }
 }
+
+/*
+PROBLEM: Given n, return nth number in fibonnacci sequence. The sequence starts with 0, 1,
+and each of the following numbers is the sum of the previous two.
+
+APPROACH:
+N is passed into the function
+build a loop, that starts with a = 0, and b = 1, and goes until n is reached.
+  on each pass, c = a + b, a = b, b = c
+*/
 
 if (require.main === module) {
   // add your own tests in here
@@ -20,5 +45,3 @@ if (require.main === module) {
 
 module.exports = fibonacci;
 
-// Please add your pseudocode to this file
-// And a written explanation of your solution
